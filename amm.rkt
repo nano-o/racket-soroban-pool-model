3a17d6fa2032881077c4890723d8078413c6d464
Giuliano Losa <giuliano@losa.fr>
2022-11-24 13:22:37 -0800

    WIP model of Soroban liquidity pool

--- amm.rkt
+++ amm.rkt
@@ -1,80 +1,123 @@
 #lang rosette
 
+; This is a model of the Soroban liquidity pool.
+; The goal is exhibit evidence that money cannot be stolen from the pool.
+
 (require
   "checked-ops.rkt")
-
-; the state of the amm: we have the amounts of the two tokens (x and y) and the number of liquidity tokens
-(struct amm (x y l) #:transparent)
 
 (define w 4) ; bit width
 (define dw (* 2 w)) ; double the bit width
 
-; ab/c without overflow in intermediate computation
-(define (ab-div-c a b c)
+; State of the pool
+; We have the pool address, reserve amounts and the number of pool shares
+(struct pool (addr reserve-a reserve-b total-shares) #:transparent)
+; Token-contract state: map from address to balance
+(struct token (balance))
+; Model state: consists of the state of the pool and of the three tokens involved
+(struct state (pool ta tb ts))
+
+(define (burn t addr amount)
+  (do
+    [new-amount <- (checked-sub ((token-balance t) addr) amount)]
+    (just
+      (λ (a)
+         (cond
+           [(bveq a addr)
+            new-amount]
+           [else
+            ((token-balance t) a)])))))
+
+(define (mint t addr amount)
+  (do
+    [new-amount <- (checked-add ((token-balance t) addr) amount)]
+    (just
+      (λ (a)
+         (cond
+           [(bveq a addr)
+            new-amount]
+           [else
+            ((token-balance t) a)])))))
+
+(define (transfer t from to amount)
+  (do
+    [new-amount-from <- (checked-sub ((token-balance t) from) amount)]
+    [new-amount-to <- (checked-add ((token-balance t) to) amount)]
+    (just
+      (λ (a)
+         (cond
+           [(bveq a from)
+            new-amount-from]
+           [(bveq a to)
+            new-amount-to]
+           [else
+            ((token-balance t) a)])))))
+
+; x*y/z without overflow in intermediate computation
+; Returns `nothing` if the result does not fit in a word
+; NOTE This uses double-width multiplication
+(define (xy/z x y z)
   (define res
     (bvudiv
       (bvmul
-        (zero-extend a (bitvector dw))
-        (zero-extend b (bitvector dw)))
-      (zero-extend c (bitvector dw))))
+        (zero-extend x (bitvector dw))
+        (zero-extend y (bitvector dw)))
+      (zero-extend z (bitvector dw))))
   (cond
-    [(bvugt
-       res
-       (zero-extend (bv -1 w) (bitvector dw)))  ; doesn't fit in a word
+    [(not ; doesn't fit in a word
+       (bveq
+         ((bvlshr res (bv w dw)))
+         (bv 0 dw)))
      nothing]
     [else
       (just (extract (- w 1) 0 res))]))
 
-; we send delta-x and delta-y to the pool and get liquidity shares in exchange
-(define (add-liquidity pool delta-x delta-y)
-  ; token quantities in the pool must be non-zero
-  (define maybe-shares-x
-    (ab-div-c delta-x (amm-l pool) (amm-x pool)))
-  (define maybe-shares-y
-    (ab-div-c delta-y (amm-l pool) (amm-y pool)))
-  (define maybe-l
+(define (deposit s from)
+  (define p (state-pool s))
+  (define ta (state-ta s))
+  (define tb (state-tb s))
+  (define ts (state-ts s))
+  (define addr
+    (pool-addr p))
+  (define balance-a
+    ((token-balance ta) addr))
+  (define balance-b
+    ((token-balance tb) addr))
+  (define total-shares
+    (pool-total-shares p))
+  (define new-total-shares
     (do
-      [sx <- maybe-shares-x]
-      [sy <- maybe-shares-y]
-      (define delta-l (bvumin sx sy))
-      (checked-add (amm-l pool) delta-l)))
+      [shares-a <- (xy/z balance-a total-shares (pool-reserve-a p))]
+      [shares-b <- (xy/z balance-b total-shares (pool-reserve-b p))]
+      (just
+        (bvumin shares-a shares-b))))
+  (define new-pool
+    (do
+      [shares <- new-total-shares]
+      (just
+        (pool
+          addr
+          balance-a
+          balance-b
+          shares))))
+  (define shares-to-mint
+    (do
+      [shares <- new-total-shares]
+      (just (bvsub new-total-shares total-shares)))) ; NOTE there's no need for checked-sub here
   (do
-    [x <- (checked-add (amm-x pool) delta-x)] [y <- (checked-add (amm-y pool) delta-y)]
-    [l <- maybe-l] ; here it'd be nice to use map and apply (Haskell <*> and <$>), but we'd need to curry bvumin, which Rosette does not support
-    (just (amm x y l))))
-
-; buggy version of the Soroban pool contract
-(define (add-liquidity-buggy pool delta-x delta-y)
-  ; token quantities in the pool must be non-zero
-  (define maybe-shares-x
-    (do
-      [x <- (checked-add (amm-x pool) delta-x)]
-      (ab-div-c x (amm-l pool) (amm-x pool))))
-  (define maybe-shares-y
-    (do
-      [y <- (checked-add (amm-y pool) delta-y)]
-      (ab-div-c y (amm-l pool) (amm-y pool))))
-  (define maybe-l
-    (do
-      [sx <- maybe-shares-x]
-      [sy <- maybe-shares-y]
-      (define delta-l (bvumin sx sy))
-      (checked-add (amm-l pool) delta-l)))
-  (do
-    [x <- (checked-add (amm-x pool) delta-x)]
-    [y <- (checked-add (amm-y pool) delta-y)]
-    [l <- maybe-l] ; here it'd be nice to use map and apply (Haskell <*> and <$>), but we'd need to curry bvumin, which Rosette does not support
-    (just (amm x y l))))
-
+    [new-pool <- new-pool]
+    [shares-to-mint <- shares-to-mint]
+    (just (state new-pool ta tb (mint ts from shares-to-mint)))))
 
 (module+ test
   (require rackunit)
   (test-case
-    "add-liquidity"
+    "deposit tests"
+    ; TODO
     (define my-pool
       (amm (bv 1 w) (bv 4 w) (bv 2 w)))
     (check-equal?
-      (add-liquidity my-pool (bv 1 w) (bv 5 w))
+      (deposit my-pool (bv 1 w) (bv 5 w))
       (just (amm (bv 2 w) (bv 9 w) (bv 4 w))))
     (check-equal?
       (add-liquidity my-pool (bv 1 w) (bv 4 w))
@@ -83,24 +126,52 @@
       (add-liquidity my-pool (bv -1 w) (bv 1 w))
       nothing)))
 
-(define (withdraw pool delta-l)
-  (define maybe-x
+(define (withdraw s to)
+  (define p (state-pool s))
+  (define ta (state-ta s))
+  (define tb (state-tb s))
+  (define ts (state-ts s))
+  (define addr
+    (pool-addr p))
+  (define balance-a
+    ((token-balance ta) addr))
+  (define balance-b
+    ((token-balance tb) addr))
+  (define balance-shares
+    ((token-balance tb) addr))
+  (define out-a
+    (xy/z balance-a balance-shares (pool-total-shares p)))
+  (define out-b
+    (xy/z balance-b balance-shares (pool-total-shares p)))
+  (define new-total-shares
+    (bvsub (pool-total-shares p) balance-shares)) ; NOTE no need for checked-sub here
+  (define new-ts
+    (burn ts addr balance-shares))
+  (define new-ta
+    (transfer ta addr to out-a))
+  (define new-tb
+    (transfer tb addr to out-b))
+  (define new-pool
     (do
-      [delta-x <- (ab-div-c delta-l (amm-x pool) (amm-l pool))]
-      (checked-sub (amm-x pool) delta-x)))
-  (define maybe-y
-    (do
-      [delta-y <- (ab-div-c delta-l (amm-y pool) (amm-l pool))]
-      (checked-sub (amm-y pool) delta-y)))
+      [out-a <- out-a]
+      [out-b <- out-b]
+      (just
+        (pool
+          addr
+          (bvsub balance-a out-a) ; NOTE no need for checked sub
+          (bvsub balance-b out-b) ; NOTE no need for checked sub
+          new-total-shares))))
   (do
-    [x <- maybe-x]
-    [y <- maybe-y]
-    [l <- (checked-sub (amm-l pool) delta-l)]
-    (just (amm x y l))))
+    [new-pool <- new-pool]
+    [new-ts <- new-ts]
+    [new-ta <- new-ta]
+    [new-tb <- new-tb]
+    (just (state new-pool new-ta new-tb new-ts))))
 
 (module+ test
   (test-case
-    "remove-liquidity"
+    "withdraw tests"
+    ; TODO
     (define my-pool
       (amm (bv 1 w) (bv 4 w) (bv 2 w)))
     (check-equal?
@@ -110,13 +181,13 @@
       (withdraw my-pool (bv -1 w))
       nothing)))
 
-(define (add-liquidity* f p xys)
-  ; f is the add-liquidity function
+; TODO sequence of deposit and withdraws, not just deposits
+(define (deposit* p xys)
   (foldl
     (λ (xy maybe-p)
        (do
          [p <- maybe-p]
-         (f p (car xy) (cdr xy))))
+         (deposit p (car xy) (cdr xy))))
     (just p) ; init
     xys))
 
